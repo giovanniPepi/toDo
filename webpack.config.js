529efd687d2acd7b6b04f0fb38b2901f3908943c
gtpepi <gtpepi@hotmail.com>
2022-04-22 21:16:04 -0300

    Update css structure

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,11 @@ module.exports = {
       {
         test: /\.(png|jpg|svg|jpeg)$/i,
         type: 'asset/resource',
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
