7656f66e0545d2ea2e22e224d34abb830bd3914c
gtpepi <gtpepi@hotmail.com>
2022-04-24 13:41:22 -0300

    Move header creation to domLoader

---
+++ src/domLoader.js
@@ -0,0 +1,27 @@
+//calls modules that handle DOM manipulation
+const domLoader = (() => {
+  const body = document.querySelector('body');
+
+  // creates header
+  const getHeader = (() => {
+    const header = document.createElement('div');
+    header.setAttribute('class', 'header');
+
+    const title = document.createElement('div');
+    title.setAttribute('class', 'title');
+    title.textContent = 'EasyList';
+
+    const save = document.createElement('div');
+    save.setAttribute('class', 'options');
+    save.setAttribute('id', 'save');
+    save.textContent = 'save';
+
+    header.appendChild(title)
+    header.appendChild(save);
+
+    body.prepend(header);
+  })();
+ 
+})();
+
+export default domLoader;
