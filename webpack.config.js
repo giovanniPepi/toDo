8db1a9fa738a4be6d5fd1542e1849ef5a1561a48
gtpepi <gtpepi@hotmail.com>
2022-04-24 10:19:21 -0300

    Update background colors, button decorations

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,12 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
     ],
   },
   plugins: [
