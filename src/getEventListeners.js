d6b01b88a1a01520c67c6558a317704b37e550b4
gtpepi <gtpepi@hotmail.com>
2022-04-24 21:56:41 -0300

    Update getEventListeners to reach all list items

--- src/getEventListeners.js
+++ src/getEventListeners.js
@@ -1,13 +1,17 @@
 const getEventListeners = () => {
 
-  const listCheck = document.querySelector('.listCheck');
+  const listCheck = document.querySelectorAll('.listCheck');
 
   //toggle class, cosmetic only
   const evaluateCheckList = (e) => {
     e.target.classList.toggle('listCheck-teste');    
   };
   
-  listCheck.addEventListener('click', e => evaluateCheckList(e));
+  listCheck.forEach((item => {
+    item.addEventListener('click', e => evaluateCheckList(e));
+  }))
+  
+
 };
 
 export default getEventListeners;
