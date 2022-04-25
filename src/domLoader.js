0f747617bca1594965d223542c493c0009360447
gtpepi <gtpepi@hotmail.com>
2022-04-24 21:32:59 -0300

    Move eventlisteners to its own module

--- src/domLoader.js
+++ src/domLoader.js
@@ -1,19 +1,12 @@
 import getHeader from "./getHeader";
 import getContainer from "./containerLoader";
+import getEventListeners from "./getEventListeners";
 
 //calls modules that handle DOM manipulation
 const domLoader = () => {
   getHeader();
   getContainer(); 
-
-  const listCheck = document.querySelector('.listCheck');
-
-  const evaluateCheckList = (e) => {
-    e.target.classList.toggle('listCheck-teste');    
-
-  };
-  
-  listCheck.addEventListener('click', e => evaluateCheckList(e));
+  getEventListeners();
 
 
 };
--- src/getEventListeners.js
+++ src/getEventListeners.js
@@ -1,5 +1,13 @@
 const getEventListeners = () => {
 
+  const listCheck = document.querySelector('.listCheck');
+
+  const evaluateCheckList = (e) => {
+    e.target.classList.toggle('listCheck-teste');    
+
+  };
+  
+  listCheck.addEventListener('click', e => evaluateCheckList(e));
 };
 
 export default getEventListeners;
